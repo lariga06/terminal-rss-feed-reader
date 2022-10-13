ea2fba4fb77d55012d2cf0a323518ec085ba50f7
Laurent A <lariga@gmail.com>
2022-10-13 14:12:38 +0700

    Version 1.0

--- simple_rss_reader.py
+++ simple_rss_reader.py
@@ -1,17 +1,33 @@
 import feedparser
+import json
+import re
 
 # https://feedparser.readthedocs.io/en/latest/
-
-rss_feed = r'https://lincolnproject.libsyn.com/rss'
+# https://lincolnproject.libsyn.com/rss
+rss_feed = input('Please enter the RSS link: ')
 
 d = feedparser.parse(rss_feed)
 
-print(d.feed.title)
+for i in range(len(d.entries)):
 
-print(d.feed.link)
+    title=json.dumps(d.entries[i].title, indent=4)
+    title=re.sub(r'(\\u2019)',"'", title).strip('"')
+    print(f'TITLE: {title}')
 
-print(d.feed.description)
+    summary = json.dumps(d.entries[i].summary, indent=4)
+    summary = re.sub(r'(<.*?>)','', summary)
+    summary = re.sub(r'(\\u2019)',"'", summary).strip('"')
+    print(f'SUMMARY: {summary}')
 
-print(d.feed.published)
+    link = json.dumps(d.entries[i]['links'][0].href, indent=4).strip('"')
+    print(f'LINK: {link}')
+    print('\n')
 
-print(d.feed.published_parsed)
+
+# print(d.feed.title)
+# print(d.feed.link)
+# print(d.feed.description)
+# print(d.feed.published)
+# print(d.feed.published_parsed)
+# print(d.entries[0])
+# print(json.dumps(d.entries[0], indent=4))
